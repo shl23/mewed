84e650098508df3e10f6139b33fd442fc41c6460
shl23 <73346790+shl23@users.noreply.github.com>
2025-04-26 15:48:57 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,40 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'https://shl323.pro/admin/');
+define('HTTP_CATALOG', 'https://shl323.pro/');
+
+// DIR
+define('DIR_OPENCART', '/home/jarettle/public_html/');
+define('DIR_APPLICATION', DIR_OPENCART . 'admin/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', '/home/jarettle/ocartdata/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'jarettle_ocar320');
+define('DB_PASSWORD', 'IS.3p845l[');
+define('DB_DATABASE', 'jarettle_ocar320');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oci4_');
+
+define('DB_SSL_KEY', '');
+define('DB_SSL_CERT', '');
+define('DB_SSL_CA', '');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
